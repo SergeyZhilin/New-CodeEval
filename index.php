b1b41cb0b8966c45e3f51a8bb06d31a4d470eaab
SimplyWorld <szhilin87@gmail.com>
2017-06-01 19:33:17 +0300

    first commit, new eval

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>A-Level</title>
+	
+	<style>#container {width:100%; text-align:center;}</style>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+	  <?php 
+	  	// if (empty($_POST['submit'])) {}
+	  	if (empty($_POST['textarea'])) {}
+	  		else{
+	  			$file = "message.txt";
+	  			$file = fopen($file, "a");
+	  			$text = $_POST['textarea']."\n";
+	  			$result = fwrite($file, $text);
+	  			unset($result);
+	  		}
+	  		
+	  		$mes = "message.txt";
+	  		$mes = fopen($mes, "r");
+	  		if ($mes ) {
+	  			# code...
+	  		}
+	  ?>
+  <form method="post">
+	<div id="container">
+		<textarea name="textarea" type="textarea" cols="100" rows="20"></textarea>
+		<input type="submit" name="submit" class="btn btn-success"> 
+  	</div>
+  </form>
+   
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
