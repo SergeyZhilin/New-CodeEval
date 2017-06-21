95ded522aad0bda99d59c8a1e70d23e27246987e
SimplyWorld <szhilin87@gmail.com>
2017-06-21 10:39:50 +0300

    check this please

--- index.php
+++ index.php
@@ -11,13 +11,23 @@
   </head>
   <body>
 	  <?php
+          if (empty($_POST['textarea'])) {
+            echo "<h1><font color='blue'><center>Hi! Lets go check your code ;)</center></font></h1>";
+          } else {
+
 	  			$file = fopen("message.txt", "w");
-                $text = $_POST['textarea']."\r";
+                $text = $_POST['textarea'];
                 ob_start();
                 eval($text);
+
                 $res = ob_get_contents();
+                $clean = ob_end_clean();
+                
 	  			$result = fwrite($file, $res);
+          unset($result);
+
 	  			fclose($file);
+        
 
 	  		$mes = "message.txt";
 	  		$mes = file_get_contents($mes);
@@ -26,9 +36,9 @@
 	  		$main = file_get_contents($main);
 
 	  		if ($main !== $mes) {
-	  		    echo "So BAd...((((";
-           } else echo "All nice";
-
+	  		    echo "<h1><font color='red'><center>Something wrong, check your code ((((</center></font></h1>";
+           } else echo "<h1><font color='green'><center>All nice.... ))))</center></font></h1>";
+         }
 	  ?>
       <h2>Числа для проверки: Fizz- 2; Buzz - 5; Общая длина - 45</h2>
   <form method="post">
